a4c12a2de99482558996a38bef68f49faac3a15d
Stopkin Sergey <stopkin.sergey@gmail.com>
2022-05-05 02:51:29 +0600

    Added XCARGS option

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function run() {
     );
     process.env.BUILD_SDK = core.getInput("build-sdk");
     process.env.BUILD_DESTINATION = core.getInput("build-destination");
+    process.env.XCARGS = core.getInput("xcargs");
 
     // Execute build.sh
     await exec.exec(`bash ${__dirname}/../build.sh`);
